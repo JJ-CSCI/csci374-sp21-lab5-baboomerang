96488d126965e5b8520dbf3972c3600af5acc857
Ilan Rodriguez <irodrigue101@gmail.com>
2021-03-22 21:36:14 +0000

    completed the first test

--- main.fs
+++ main.fs
@@ -1,11 +1,15 @@
 module Assignment
 
 type Tree =
-    | Node of Tree * Tree
+    | Node of Tree * Tree //tuple of two
     | Leaf of int
 
 let rec prod (t:Tree) :int =
-    0
+    match t with   //the concept clicked for me I think
+    //| Tree * Tree -> 
+    | Node (left, right) -> prod left * prod right
+    | Leaf l -> l
+    | _ -> 1  //identity
 
 let rec map (f:int->int) (t:Tree) :Tree =
     t
